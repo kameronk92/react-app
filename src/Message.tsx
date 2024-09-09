20b94bbd921883254402a7bd1a09608ede8e215a
Kam Kennedy <kameronk013@gmail.com>
2024-09-09 13:12:51 -0600

    add constant

--- src/Message.tsx
+++ src/Message.tsx
@@ -1,5 +1,10 @@
 function Message() {
-  return <h1>Hello World</h1>
+
+  const name = 'AI Overlords'
+  if (name)
+    return <h1>Hello {name}</h1>
+  return <h1>Hello world</h1>
+
 }
 
 export default  Message;
